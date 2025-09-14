1366ae3469a1d0c5769bc1a7e4d7feae97ac2fad
karljoshua101202-2004 <karljoshua101202@gmail.com>
2025-09-15 02:11:24 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = "shelter-locator-cache-v1";
+const urlsToCache = [
+  "index.html",
+  "manifest.json",
+  "icon-maskable-192.png",
+  "icon-maskable-512.png",
+  "https://unpkg.com/leaflet@1.9.4/dist/leaflet.css",
+  "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js",
+  "https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.css",
+  "https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
